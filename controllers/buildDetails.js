dd4a9ac8742f8260ee5d4e951b15dbf371f5d3a7
David House <david.house@kp.org>
2019-10-02 21:40:47 -0400

    :sparkles: Updates to all pages

--- controllers/buildDetails.js
+++ controllers/buildDetails.js
@@ -1,13 +1,25 @@
 /**
  * handle buildDetails
- * @param {*} req 
- * @param {*} res 
+ * @param {*} req
+ * @param {*} res
  * @param {*} cache
  * @param {*} db
- * @param {*} path 
+ * @param {*} path
  */
 async function handle(req, res, cache, db, path) {
-  res.render(path + 'buildDetails', {build: {}})
+  console.dir(req.query)
+  const build = await db.fetchBuild(req.query.owner, req.query.repository, req.query.buildKey, req.query.build)
+  const buildTasks = await db.fetchBuildTasks(req.query.owner, req.query.repository,
+    req.query.buildKey, req.query.build)
+  const buildDetails = build.rows.length > 0 ? build.rows[0] : {}
+  const tasks = []
+  for (let index = 0; index < buildTasks.rows.length; index++) {
+    const taskDetails = await cache.fetchTaskConfig(buildTasks.rows[index].taskid)
+    const task = buildTasks.rows[index]
+    task.title = taskDetails.title
+    tasks.push(task)
+  }
+  res.render(path + 'buildDetails', {build: buildDetails, tasks: tasks})
 }
 
 module.exports.handle = handle
