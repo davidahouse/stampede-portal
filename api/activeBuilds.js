eb5ba10afb65b7c7f2def41a608114850c7a1cfb
David House <davidahouse@gmail.com>
2019-10-08 18:34:04 -0400

    :rotating_light: lint fixes

--- api/activeBuilds.js
+++ api/activeBuilds.js
@@ -2,28 +2,28 @@
 
 /**
  * handle activeBuilds
- * @param {*} req 
- * @param {*} res 
- * @param {*} serverConf 
- * @param {*} cache 
+ * @param {*} req
+ * @param {*} res
+ * @param {*} serverConf
+ * @param {*} cache
  * @param {*} db
  */
 async function handle(req, res, serverConf, cache, db) {
-    const activeBuilds = await cache.fetchActiveBuilds()
-    console.dir(activeBuilds)
-    const builds = []
-    for (let index = 0; index < activeBuilds.length; index++) {
-        const buildID = activeBuilds[index]
-        const buildDetails = await db.fetchBuild(buildID)
-        const tasks = await db.fetchBuildTasks(buildID)
-        builds.push({
-            buildID: buildID,
-            buildDetails: (buildDetails != null && buildDetails.rows.length > 0) ?
+  const activeBuilds = await cache.fetchActiveBuilds()
+  console.dir(activeBuilds)
+  const builds = []
+  for (let index = 0; index < activeBuilds.length; index++) {
+    const buildID = activeBuilds[index]
+    const buildDetails = await db.fetchBuild(buildID)
+    const tasks = await db.fetchBuildTasks(buildID)
+    builds.push({
+      buildID: buildID,
+      buildDetails: (buildDetails != null && buildDetails.rows.length > 0) ?
                 buildDetails.rows[0] : {},
-            tasks: tasks.rows,
-        })
-    }
-    res.send(builds)
+      tasks: tasks.rows,
+    })
+  }
+  res.send(builds)
 }
 
 module.exports.handle = handle
