aa243d94a75602f64b0f010ecb7fe1b899187251
David House <davidahouse@gmail.com>
2019-10-20 14:32:36 -0400

    :sparkles: Config defaults REST API

---
+++ api/admin/configDefaults.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * handle
+ * @param {*} req
+ * @param {*} res
+ * @param {*} serverConf
+ * @param {*} cache
+ * @param {*} db
+ */
+async function handle(req, res, serverConf, cache, db) {
+  const defaults = await cache.fetchSystemDefaults();
+  res.send(defaults.defaults);
+}
+
+module.exports.handle = handle;
