b3a38c8de9f377165b0c6dec13557045b48ca9f6
David House <david.house@kp.org>
2019-10-20 09:28:36 -0400

    :recycle: Refactor of portal layout and stubbed pages

---
+++ controllers/admin/tasks.js
@@ -0,0 +1,24 @@
+/**
+ * handle tasks
+ * @param {*} req
+ * @param {*} res
+ * @param {*} cache
+ * @param {*} db
+ * @param {*} path
+ */
+async function handle(req, res, cache, db, path) {
+  const taskList = await cache.fetchTasks();
+  const sortedTasks = taskList.sort();
+  const tasks = [];
+  for (let index = 0; index < sortedTasks.length; index++) {
+    const taskDetails = await cache.fetchTaskConfig(sortedTasks[index]);
+    tasks.push({
+      id: sortedTasks[index],
+      title: taskDetails.title
+    });
+  }
+  console.dir(tasks);
+  res.render(path + "admin/tasks", { tasks: tasks });
+}
+
+module.exports.handle = handle;
