a317f95883139148af63c2dc7a5eba0d10231f04
David house <davidahouse@gmail.com>
2019-10-24 20:25:16 -0600

    :sparkles: Default config list in admin

--- controllers/admin/defaults.js
+++ controllers/admin/defaults.js
@@ -7,7 +7,17 @@
  * @param {*} path
  */
 async function handle(req, res, cache, db, path) {
-  res.render(path + "admin/defaults", {});
+  const defaults = await cache.fetchSystemDefaults();
+  const configDefaults = [];
+  console.dir(defaults.defaults);
+  Object.keys(defaults.defaults).forEach(function(key) {
+    configDefaults.push({
+      key: key,
+      value: defaults.defaults[key]
+    });
+  });
+  console.dir(configDefaults);
+  res.render(path + "admin/defaults", { defaults: configDefaults });
 }
 
 module.exports.handle = handle;
