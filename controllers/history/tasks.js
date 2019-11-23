64bec74e6aed2b2692a4c24e98efa5d2d9d6ed3e
David House <david.house@kp.org>
2019-11-22 21:33:49 -0500

    :sparkles: Added filtering to task history screen

--- controllers/history/tasks.js
+++ controllers/history/tasks.js
@@ -7,8 +7,47 @@
  * @param {*} path
  */
 async function handle(req, res, cache, db, path) {
-  const tasks = await db.recentTasks(8);
-  res.render(path + "history/tasks", { tasks: tasks.rows });
+  let timeFilter = "Last 8 hours";
+  if (req.query.time != null) {
+    timeFilter = req.query.time;
+  }
+
+  let taskFilter = "All";
+  if (req.query.task != null) {
+    taskFilter = req.query.task;
+  }
+
+  const taskList = await cache.fetchTasks();
+  const sortedTasks = taskList.sort();
+  sortedTasks.unshift("All");
+
+  let repositoryFilter = "All";
+  if (req.query.repository != null) {
+    repositoryFilter = req.query.repository;
+  }
+
+  const repositoriesRows = await db.fetchRepositories();
+  const repositories = [];
+  repositories.push("All");
+  for (let index = 0; index < repositoriesRows.rows.length; index++) {
+    repositories.push(
+      repositoriesRows.rows[index].owner +
+        "/" +
+        repositoriesRows.rows[index].repository
+    );
+  }
+
+  const tasks = await db.recentTasks(timeFilter, taskFilter, repositoryFilter);
+
+  res.render(path + "history/tasks", {
+    tasks: tasks.rows,
+    timeFilter: timeFilter,
+    timeFilterList: ["Last 8 hours", "Today", "Yesterday"],
+    taskFilter: taskFilter,
+    taskList: sortedTasks,
+    repositoryFilter: repositoryFilter,
+    repositoryList: repositories
+  });
 }
 
 module.exports.handle = handle;
