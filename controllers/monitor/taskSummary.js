cf72e6dba9da6662faa6c820d1f52fe84a07adc1
David house <davidahouse@gmail.com>
2019-11-24 18:41:51 -0500

    :sparkles: task summary stats

--- controllers/monitor/taskSummary.js
+++ controllers/monitor/taskSummary.js
@@ -47,6 +47,15 @@ async function handle(req, res, cache, db, path) {
     const taskLabels = [];
     const taskData = [];
     const taskColor = [];
+    const summary = {
+      total: 0,
+      success: 0,
+      failure: 0,
+      totalDuration: 0,
+      avgDuration: 0,
+      minDuration: 999999,
+      maxDuration: 0
+    };
 
     for (let tindex = 0; tindex < tasks.rows.length; tindex++) {
       if (
@@ -56,15 +65,29 @@ async function handle(req, res, cache, db, path) {
       ) {
         taskLabels.push(tasks.rows[tindex].task_id);
         if (tasks.rows[tindex].conclusion === "success") {
+          summary.success = summary.success + 1;
           taskColor.push("rgba(0, 255, 0, 0.6)");
         } else {
+          summary.failure = summary.failure + 1;
           taskColor.push("rgba(255, 0, 0, 0.6)");
         }
-        taskData.push(
+        const duration =
           (tasks.rows[tindex].finished_at - tasks.rows[tindex].started_at) /
-            1000.0
-        );
+          1000.0;
+        taskData.push(duration);
+        summary.total = summary.total + 1;
+        summary.totalDuration = summary.totalDuration + duration;
+        if (summary.minDuration > duration) {
+          summary.minDuration = duration;
+        }
+        if (summary.maxDuration < duration) {
+          summary.maxDuration = duration;
+        }
       }
+    }
+
+    if (summary.total > 0) {
+      summary.avgDuration = summary.totalDuration / summary.total;
     }
 
     let data = {
@@ -80,7 +103,8 @@ async function handle(req, res, cache, db, path) {
 
     graphs.push({
       task: sortedTasks[index],
-      data: data
+      data: data,
+      summary: summary
     });
   }
 
